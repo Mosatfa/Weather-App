dc6e6e173983d814afd07d4553b7841f1635aaf5
Mostafa <105412165+Mosatfa@users.noreply.github.com>
2022-09-28 12:30:35 +0200

    Add files via upload

---
+++ js/mine.js
@@ -0,0 +1,93 @@
+let locationCountry = document.querySelector(".locationCountry")
+    forecastTodayDegree = document.querySelector(".forecastTodayDegree")
+    todayIcon = document.querySelector(".today-icon")
+    todayDate = document.querySelector(".todayDate")
+    stateToday = document.querySelector(".stateToday")
+    upadted = document.querySelector(".upadted")
+    humidity = document.querySelector(".humidity")
+    wind = document.querySelector(".wind")
+    visibility = document.querySelector(".visibility")
+    searchBar = document.querySelector(".searchBar")
+    reloadicon = document.querySelector(".reloadicon")
+
+  
+
+let responseData
+let date = new Date()
+weekDays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+monthName = ['January','February','March','April','May','June','July','August','September','October','Novmber','December'];
+
+
+async function getWeather(currentCity = "cairo"){
+    let apiResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6f36647865554305a9e12939221106&q=${currentCity}&days=3&aqi=no&alerts=no`)
+    responseData = await apiResponse.json()
+    displayTodayForecast()
+    displayNextdayForecast()
+}
+getWeather()
+
+
+function displayTodayForecast(){
+    locationCountry.innerHTML = `${responseData.location.name}, ${responseData.location.country}`
+    todayIcon.setAttribute("src" , `https:${responseData.current.condition.icon}`)
+    forecastTodayDegree.innerHTML = responseData.current.temp_c
+    todayDate.innerHTML = `${weekDays[date.getDay()]} ${date.getDate()} ${monthName[date.getMonth()]} ${date.getFullYear()}`
+    stateToday.innerHTML = responseData.current.condition.text
+    upadted.innerHTML = " Updated as of " + parseFloat(responseData.current.last_updated.slice(-6)) % 12 + responseData.current.last_updated.slice(-3) + `<i onclick="reloadPage()" class="reloadicon fa-solid ms-1 fs-6 fa-rotate-right text-white"></i>`
+    humidity.innerHTML = `Humidity ${responseData.current.humidity}%`
+    wind.innerHTML = `Wind ${responseData.current.wind_kph} km/h`
+    visibility.innerHTML = `Visibility ${responseData.current.vis_km} km/h`
+
+    let imgs = responseData.current.condition.icon.split("/")[5]
+    if(imgs === "night"){
+      document.body.style.backgroundImage = "url('./imges/wallpaperflare.com_wallpaper\ \(12\).jpg')"
+      searchBar.style.backgroundColor = "transparent"
+      searchBar.style.color = "#fff";
+    }
+    else{
+      document.body.style.backgroundImage = "url('./imges/wallpaperflare.com_wallpaper\ \(8\).jpg')";
+      searchBar.style.backgroundColor = "#fff";
+      searchBar.style.color = "#212529";
+    }
+}
+
+
+
+
+
+function displayNextdayForecast(){
+    let cards =``
+    for(let i =0; i < 2; i++){
+        let nDay = responseData.forecast.forecastday[i+1].date
+        let days = nDay.split("-")[2]
+        cards += `<div class="col-md-4">
+        <div class="card p-2">
+          <h4 class="nextDay ms-3">${getNextDay(nDay)} ${days}</h4>
+          <img class="text-start mb-2" src="https:${responseData.forecast.forecastday[i+1].day.condition.icon}" width="60px" alt="forecastNextDayIcon">
+          <div class="maxDegreeNextDay d-flex  ms-3">
+            <div class="d-flex"><h3 class="forecastNextDayDegree h4" title="Max Degree">${responseData.forecast.forecastday[i+1].day.maxtemp_c}</h3><h3><sup>o</sup></h3></div>
+            <div class="d-flex"><h3 class="minDgree text-muted h6 m-0 ms-1 mt-2 " title="Min Degree">${responseData.forecast.forecastday[i+1].day.mintemp_c}<sup>o</sup></h3></div>
+          </div>
+          <div class="stateNextDay ms-3">${responseData.forecast.forecastday[i+1].day.condition.text}</div>
+        </div>        
+      </div>`
+    }
+   document.querySelector(".cards").innerHTML = cards
+}
+
+
+function getNextDay(apiNextDay){
+  let days = new Date(apiNextDay)
+  return weekDays[days.getDay()]
+}
+
+getNextDay()
+
+searchBar.addEventListener("keyup",function(){
+  getWeather(this.value)
+})
+
+function reloadPage(){
+  upadted.innerHTML = " Updated as of " + parseFloat(responseData.current.last_updated.slice(-6)) % 12 + responseData.current.last_updated.slice(-3) + `<i class="fa-solid ms-1 fa-spin fa-rotate"></i>`
+  window.location.reload()
+}
